d36ad380957bcf7888f7e68ac20a38cf0aa314d9
LAB_104_PC11 <ed.fernandezr@duocuc.cl>
2023-04-22 12:42:26 -0400

    Version 1.3

---
+++ js/app.js
@@ -0,0 +1,37 @@
+/*console.log("Estamos Conectados")
+alert("Mensaje de alerta al documento html");
+document.write("<h1>Mensaje al documento desde jscript</h1>");
+*/
+/*
+var vProm = 5.3;
+if (vProm > 4) {
+    console.log("Asignatura aprobada");
+}
+else {
+    console.log("Asignatura reprobada")
+}
+*/
+var nt1 = 1.3;
+var nt2 = 2.8;
+var nt3 = 5.2;
+vsuma = 0;
+/*
+console.log("su promedio es: "+(nt1+nt2+nt3)/3);
+
+for(var x = 0; x < 3; x++){
+     console.log ("Promedio...")
+}
+*/
+//array
+
+var vArr = ["juan perez", 50, 5.7, true];
+console.log(vArr);
+console.log("Su edad es " + vArr[1]);
+for (var y = 0; y < vArr.length;y++){
+      console.log(vArr[y])
+} 
+
+while(){
+
+    console.log(vArr[y])
+}
